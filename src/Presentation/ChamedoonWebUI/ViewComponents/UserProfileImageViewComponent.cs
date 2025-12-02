12b24686775c425b69d1d939f5dc35a1dcca6280
Fara <khakpwr2@gmail.com>
2025-12-02 09:31:51 +0330

    Store customer profile images as base64

--- src/Presentation/ChamedoonWebUI/ViewComponents/UserProfileImageViewComponent.cs
+++ src/Presentation/ChamedoonWebUI/ViewComponents/UserProfileImageViewComponent.cs
@@ -1,4 +1,5 @@
-﻿using Chamedoon.Application.Services.Customers.Query;
+using Chamedoon.Application.Services.Customers;
+using Chamedoon.Application.Services.Customers.Query;
 using Chamedoon.Application.Services.Customers.ViewModel;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -19,16 +20,25 @@ namespace ChamedoonWebUI.Components
             var username = User?.Identity?.Name;
             if (string.IsNullOrWhiteSpace(username))
             {
-                return View<CustomerProfileViewModel?>(viewName, null);
+                return View<CustomerProfileViewModel>(viewName, BuildDefaultProfile());
             }
 
             var customer = await mediator.Send(new GetCustomerProfileQuery { UserName = username });
             if (!customer.IsSuccess || customer.Result is null)
             {
-                return View<CustomerProfileViewModel?>(viewName, null);
+                return View<CustomerProfileViewModel>(viewName, BuildDefaultProfile());
             }
+
+            customer.Result.ProfileImage = ProfileImageHelper.NormalizeProfileImage(customer.Result.ProfileImage);
 
             return View<CustomerProfileViewModel>(viewName, customer.Result);
         }
+
+        private static CustomerProfileViewModel BuildDefaultProfile() => new()
+        {
+            ProfileImage = ProfileImageHelper.GetDefaultBase64(),
+            FirstName = "مهمان",
+            LastName = string.Empty,
+        };
     }
 }
