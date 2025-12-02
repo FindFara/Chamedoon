0d2d720f56f5759d8c4fb436a82485ff8d4d38b3
Fara <khakpwr2@gmail.com>
2025-12-02 10:43:39 +0330

    Extract mobile menu assets

--- src/Presentation/ChamedoonWebUI/wwwroot/js/site.js
+++ src/Presentation/ChamedoonWebUI/wwwroot/js/site.js
@@ -81,28 +81,6 @@
         }
     }
 
-    const landingHamburger = document.querySelector('.landing-hamburger');
-    const landingOffcanvasElement = document.getElementById('landingMobileNav');
-
-    if (landingHamburger && landingOffcanvasElement && window.bootstrap?.Offcanvas) {
-        const landingOffcanvas = bootstrap.Offcanvas.getOrCreateInstance(landingOffcanvasElement, {
-            scroll: true,
-            backdrop: true,
-        });
-
-        if (!landingHamburger.dataset.offcanvasBound) {
-            landingHamburger.dataset.offcanvasBound = 'true';
-
-            landingHamburger.addEventListener('click', (event) => {
-                event.preventDefault();
-
-                if (!landingOffcanvasElement.classList.contains('show')) {
-                    landingOffcanvas.show();
-                }
-            });
-        }
-    }
-
     const backToTop = document.getElementById('backToTop');
     if (backToTop) {
         const toggleButtonVisibility = () => {
