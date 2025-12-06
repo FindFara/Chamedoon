bed139dfbfcd405f34373980efd51742d16166c9
Fara <khakpwr2@gmail.com>
2025-12-06 11:32:57 +0330

    Enable email code registration and login

--- src/Libraries/Chamedoon.Application/Services/Account/Register/ViewModel/RegisterUser_VM.cs
+++ src/Libraries/Chamedoon.Application/Services/Account/Register/ViewModel/RegisterUser_VM.cs
@@ -4,28 +4,21 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Chamedoon.Application.Services.Account.Register.ViewModel;
 
-public class RegisterUser_VM : IMapFrom<User>
-{
+    public class RegisterUser_VM : IMapFrom<User>
+    {
 
-    [Display(Name = "ایمیل")]
-    [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [Display(Name = "ایمیل")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
     [MaxLength(200, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
     [EmailAddress(ErrorMessage = "ایمیل وارد شده معتبر نمی باشد")]
     //[Remote("IsEmailInUse", "Account", HttpMethod = "POST")]
     public string Email { get; set; }
 
-    [Display(Name = "کلمه عبور")]
+    [Display(Name = "کد تایید")]
     [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
-    [MaxLength(200, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
-    [DataType(DataType.Password)]
-    public string Password { get; set; }
-
-    //[Display(Name = "تکرار کلمه عبور")]
-    //[Required(ErrorMessage = "لطفا {0} را وارد کنید")]
-    //[MaxLength(200, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
-    //[Compare("Password", ErrorMessage = "کلمه های عبور مغایرت دارند")]
-    //[DataType(DataType.Password)]
-    //public string RePassword { get; set; }
+    [MaxLength(4, ErrorMessage = "{0} باید چهار رقمی باشد.")]
+    [MinLength(4, ErrorMessage = "{0} باید چهار رقمی باشد.")]
+    public string VerificationCode { get; set; }
 }
 
 
