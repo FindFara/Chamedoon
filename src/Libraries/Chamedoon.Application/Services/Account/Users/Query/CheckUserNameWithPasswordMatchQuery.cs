a4c1ba7f6355f19fc365785ebc686d3274343ad6
Fara <khakpwr2@gmail.com>
2025-12-06 11:31:50 +0330

    Enable email code registration and login

--- src/Libraries/Chamedoon.Application/Services/Account/Users/Query/CheckUserNameWithPasswordMatchQuery.cs
+++ src/Libraries/Chamedoon.Application/Services/Account/Users/Query/CheckUserNameWithPasswordMatchQuery.cs
@@ -1,10 +1,6 @@
-﻿using Chamedoon.Application.Common.Interfaces;
-using Chamedoon.Application.Common.Models;
+﻿using Chamedoon.Application.Common.Models;
 using Chamedoon.Application.Services.Account.Login.ViewModel;
-using Chamedoon.Domin.Base;
-using Chamedoon.Domin.Entity.Users;
 using MediatR;
-using Microsoft.AspNetCore.Identity;
 
 namespace Chamedoon.Application.Services.Account.Users.Query;
 
@@ -16,36 +12,10 @@ public class CheckUserNameAndPasswordMatchQuery : IRequest<OperationResult<bool>
 }
 public class CheckUserNameAndPasswordMatchHandler : IRequestHandler<CheckUserNameAndPasswordMatchQuery, OperationResult<bool>>
 {
-    #region Property
-    private readonly IApplicationDbContext context;
-    private readonly SignInManager<User> signinmanager;
-    #endregion
-
-    #region Ctor
-    public CheckUserNameAndPasswordMatchHandler(IApplicationDbContext context, SignInManager<User> signinmanager)
-    {
-        this.context = context;
-        this.signinmanager = signinmanager;
-    }
-    #endregion
-
     #region Method
     public async Task<OperationResult<bool>> Handle(CheckUserNameAndPasswordMatchQuery request, CancellationToken cancellationToken)
     {
-        var loginUser = await signinmanager.PasswordSignInAsync(
-             request.UserName,
-             request.LoginUser.Password,
-             request.LoginUser.RememberMe,
-             true);
-
-        if (loginUser.IsLockedOut)
-            return OperationResult<bool>
-                .Fail("اکانت شما به دلیل ورود های ناموفق قفل شده است ، چند دقیقه دیگر دوباره امتحان کنید");
-
-        if (loginUser.Succeeded)
-            return OperationResult<bool>.Success(true);
-
-        return OperationResult<bool>.Fail("مشکلی در ورود رخ داده است ، لطفا چند دقیقه دیگر دوباره امتحان کنید");
+        return OperationResult<bool>.Fail("ورود با رمز عبور غیرفعال شده است.");
     }
 
     #endregion
